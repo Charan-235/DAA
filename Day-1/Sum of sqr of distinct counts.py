d1075b80e1c9200ccaccb5c8bec559e7d22826c4
Charan-235 <krishnacharan689@gmail.com>
2024-10-07 10:17:33 +0530

    Create Sum of sqr of distinct counts.py

---
+++ Day-1/Sum of sqr of distinct counts.py
@@ -0,0 +1,13 @@
+def sum_of_squares(nums):
+    total_sum = 0
+    for i in range(len(nums)):
+        distinct_elements = []
+        for j in range(i, len(nums)):
+            if nums[j] not in distinct_elements:
+                distinct_elements.append(nums[j])
+            total_sum += len(distinct_elements) ** 2
+    return total_sum
+nums = [1, 2, 1]
+print(sum_of_squares_of_distinct_counts(nums)) 
+
+# Output: 15
